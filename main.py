29a54af8aaa5de3e303185161dd59f7f43af7b70
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-01-27 13:35:43 +0100

    added early stopping

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import h5py
-import random
-import math
-from operator import add
 import time
 import os
 import sys
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torch.distributions import Categorical
 from torch.optim.lr_scheduler import StepLR
 
 from models.resnet import resnet50
 from utils.dataset import Dataset
+from utils.early_stopping import EarlyStopping
 
-path_data = "/data/lcz42_votes/"
+path_data = "/data/lcz42_votes/data/"
+#path_data = "D:/Data/LCZ Votes/"
 
 train_data = h5py.File(path_data + "train_data.h5",'r')
 x_train = np.array(train_data.get("x"))
@@ -53,75 +48,106 @@ idx = torch.randperm(x_test.shape[0])
 x_test = x_test[idx].view(x_test.size())
 y_test = y_test[idx].view(y_test.size())
 
-train_loader = torch.utils.data.DataLoader(Dataset(x_train, y_train), batch_size = 256, shuffle=True)
-test_loader = torch.utils.data.DataLoader(Dataset(x_test, y_test), batch_size = 256, shuffle=False)
-
-epochs = 30
+# set parameters
+n_epochs = 100
 learning_rate = 0.001
+patience = 8
+batch_size = 256
+
+train_loader = torch.utils.data.DataLoader(Dataset(x_train, y_train), batch_size = batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(Dataset(x_test, y_test), batch_size = batch_size, shuffle=False)
+
 
 optimizer = optim.Adam(params = model.parameters(), lr = learning_rate)
 criterion = nn.CrossEntropyLoss()
-
 scheduler = StepLR(optimizer, step_size=2, gamma=0.75)
 
-start_time = time.time()
+def train_model(model, batch_size, patience, n_epochs):
 
-train_losses = []
-test_losses = []
-train_correct = []
-test_correct = []
+    train_losses = []
+    test_losses = []
+    train_correct = []
+    test_correct = []
+    avg_train_losses = []
+    avg_valid_losses = []
 
-for i in range(epochs):
-    trn_corr = 0
-    tst_corr = 0
+    early_stopping = EarlyStopping(patience=patience, verbose=True)
 
-    # Run the training batches
-    for b, (X_train, Y_train) in enumerate(train_loader):
-        b += 1
-        # Learning rate decay
-        scheduler.step()
+    for i in range(epochs):
+        trn_corr = 0
+        tst_corr = 0
 
-        X_train, Y_train = X_train.to(device, dtype=torch.float), Y_train.to(device)
+        # Run the training batches
+        for b, (X_train, Y_train) in enumerate(train_loader):
+            b += 1
+            # Learning rate decay
+            scheduler.step()
 
-        # Apply the model
-        y_pred = model.forward(X_train)
-        loss = criterion(y_pred, torch.max(Y_train, 1)[1])
-
-        # Tally the number of correct predictions
-        predicted = torch.max(y_pred.data, 1)[1]
-        batch_corr = (predicted == torch.max(Y_train, 1)[1]).sum()
-        trn_corr += batch_corr
-
-        # Update parameters
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # Print interim results
-        if b % 120 == 0:
-            print(
-                f'epoch: {i + 1:2}  batch: {b:4} [{256 * b:6}/{len(x_train):6}]  loss: {loss.item():10.8f} accuracy: {trn_corr.item() * 100 / (256 * b):7.3f}%')
-
-    train_losses.append(loss)
-    train_correct.append(trn_corr)
-
-    # Run the testing batches
-    with torch.no_grad():
-        for b, (X_test, Y_test) in enumerate(test_loader):
-            X_test, Y_test = X_test.to(device, dtype=torch.float), Y_test.to(device)
+            X_train, Y_train = X_train.to(device, dtype=torch.float), Y_train.to(device)
 
             # Apply the model
-            y_val = model.forward(X_test)
+            y_pred = model.forward(X_train)
+            loss = criterion(y_pred, torch.max(Y_train, 1)[1])
 
             # Tally the number of correct predictions
-            predicted = torch.max(y_val.data, 1)[1]
-            tst_corr += (predicted == torch.max(Y_test, 1)[1]).sum()
+            predicted = torch.max(y_pred.data, 1)[1]
+            batch_corr = (predicted == torch.max(Y_train, 1)[1]).sum()
+            trn_corr += batch_corr
 
-    loss = criterion(y_val, torch.max(Y_test, 1)[1])
-    test_losses.append(loss)
-    test_correct.append(tst_corr)
+            # Update parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    print(
-        f'epoch: {i + 1:2} testing loss: {loss.item():10.8f} testing accuracy: {tst_corr.item() * 100 / len(x_test):7.3f}%')
+            train_losses.append(loss.item())
+            train_correct.append(trn_corr)
 
+        # Run the testing batches
+        with torch.no_grad():
+            for b, (X_test, Y_test) in enumerate(test_loader):
+                X_test, Y_test = X_test.to(device, dtype=torch.float), Y_test.to(device)
+
+                # Apply the model
+                y_val = model.forward(X_test)
+
+                # Tally the number of correct predictions
+                predicted = torch.max(y_val.data, 1)[1]
+                tst_corr += (predicted == torch.max(Y_test, 1)[1]).sum()
+
+                loss = criterion(y_val, torch.max(Y_test, 1)[1])
+                test_losses.append(loss)
+                test_correct.append(tst_corr)
+
+        # calculate average loss over an epoch
+        train_loss = np.average(train_losses)
+        valid_loss = np.average(test_losses)
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
+
+        print(
+            f'epoch: {i + 1:2} training loss: {train_loss:10.8f} training accuracy: {trn_corr.item() * 100 / len(x_train) :7.3f}%')
+        print(
+            f'epoch: {i + 1:2} validation loss: {valid_loss:10.8f} validation accuracy: {tst_corr.item() * 100 / len(x_test):7.3f}%')
+
+        # clear lists to track next epoch
+        train_losses = []
+        test_losses = []
+
+        early_stopping(valid_loss, model)
+
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+    # load the last checkpoint with the best model
+    model.load_state_dict(torch.load('checkpoint.pt'))
+
+    return model, avg_train_losses, avg_valid_losses
+
+start_time = time.time()
+model, train_loss, valid_loss = train_model(model, batch_size, patience, n_epochs)
 print(f'\nDuration: {time.time() - start_time:.0f} seconds')  # print the time elapsed
+
+PATH = "ResNet50_b" + batch_size + "_e_" + n_epochs + "_weightdecay"
+
+torch.save(model.state_dict(), PATH)
