7ec650343a685ea6794db96372284c69450051e2
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-01-30 20:11:45 +0100

    change lr

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import h5py
 import time
-import os
-import sys
 
 import torch
 import torch.nn as nn
@@ -50,7 +48,7 @@ y_test = y_test[idx].view(y_test.size())
 
 # set parameters
 n_epochs = 100
-learning_rate = 0.01
+learning_rate = 0.001
 patience = 20
 batch_size = 32
 
