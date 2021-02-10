4e9fbf30838c54aba5319e0d3e9cadd65d39187d
ChrisKo94 <44167684+ChrisKo94@users.noreply.github.com>
2021-02-10 17:13:10 +0100

    change savename

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ learning_rate = 0.01
 patience = 20
 batch_size = 128
 
-PATH = "/data/lcz42_votes/ResNet_LCZ/ResNet50_b" + str(batch_size) + "_e_" + str(n_epochs) + "_weightdecay"
+PATH = "/data/lcz42_votes/ResNet_LCZ/ResNet18_b" + str(batch_size) + "_e_" + str(n_epochs) + "_weightdecay"
 
 train_loader = torch.utils.data.DataLoader(Dataset(x_train, y_train), batch_size = batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(Dataset(x_test, y_test), batch_size = batch_size, shuffle=False)
